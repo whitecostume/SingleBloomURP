b19a80a5c8175d8f151dfef07141850d3871c1f3
whiteye <18351967270@163.com>
2023-07-23 01:02:59 +0800

    First Commit

---
+++ Assets/Scripts/SingleBloomRenderFeature.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class SingleBloomRenderFeature : ScriptableRendererFeature
+{
+    public Material BloomMat;
+    public int BlitStep = 7;
+
+    class SingleBloomRenderPass : ScriptableRenderPass
+    {
+        Material bloomMat;
+        int _blit_step = 7;
+        
+
+        public void Setup(Material bloomMat,int blitStep)
+        {
+            this.bloomMat = bloomMat;
+            this._blit_step = blitStep;
+        }
+
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in a performant manner.
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get("SingleBloom");
+            int width = renderingData.cameraData.cameraTargetDescriptor.width;
+            int height = renderingData.cameraData.cameraTargetDescriptor.height;
+            int downSize = 2;
+
+            int[] RT_DownsizeIds = new int[this._blit_step];
+
+            RenderTextureDescriptor descriptor = renderingData.cameraData.cameraTargetDescriptor;
+            descriptor.mipCount = 0;
+            descriptor.depthBufferBits = 0;
+            descriptor.msaaSamples = 0;
+
+            for (int i = 0; i < RT_DownsizeIds.Length; i++)
+            {
+                int w = width / downSize;
+                int h = height / downSize;
+                RenderTextureDescriptor desc = descriptor;
+                desc.width = w;
+                desc.height = h;
+                RT_DownsizeIds[i] = Shader.PropertyToID("BloomDown" + i);
+                cmd.GetTemporaryRT(RT_DownsizeIds[i], desc,FilterMode.Bilinear);
+            }
+
+            var source = renderingData.cameraData.renderer.cameraColorTarget;
+            cmd.Blit(source,RT_DownsizeIds[0],bloomMat,0);
+            for (int i = 1; i < RT_DownsizeIds.Length;i++)
+            {
+                
+            }
+
+        }
+
+        // Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+        }
+    }
+    
+    SingleBloomRenderPass m_ScriptablePass;
+
+    /// <inheritdoc/>
+    public override void Create()
+    {
+        m_ScriptablePass = new SingleBloomRenderPass();
+
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScriptablePass.Setup(this.BloomMat, this.BlitStep);
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
